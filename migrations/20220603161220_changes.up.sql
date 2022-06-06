a2fa8ea9d3b6d7c8ade27f525453bc3b11154ec3
alex <alex.gutan.m@gmail.com>
2022-06-06 12:05:38 +0300

    commit migrations

---
+++ migrations/20220603161220_changes.up.sql
@@ -0,0 +1,6 @@
+-- create "links" table
+CREATE TABLE `links` (`id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, `short_path` text NOT NULL, `long_uri` text NOT NULL, `accessed_times` integer NOT NULL DEFAULT 0, `created_at` datetime NOT NULL);
+-- create index "links_short_path_key" to table: "links"
+CREATE UNIQUE INDEX `links_short_path_key` ON `links` (`short_path`);
+-- create index "links_long_uri_key" to table: "links"
+CREATE UNIQUE INDEX `links_long_uri_key` ON `links` (`long_uri`);
---
+++ migrations/20220603161409_changes.down.sql
@@ -0,0 +1,6 @@
+-- reverse: create index "links_long_uri_key" to table: "links"
+DROP INDEX `links_long_uri_key`;
+-- reverse: create index "links_short_path_key" to table: "links"
+DROP INDEX `links_short_path_key`;
+-- reverse: create "links" table
+DROP TABLE `links`;
